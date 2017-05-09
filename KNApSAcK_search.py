417429f1da0ff96f3ce8829e65dc3828bd038df1
sato-masayukidesu <maatanbeta@gmail.com>
2017-05-09 10:00:13 +0900

    途中経過

--- KNApSAcK_search.py
+++ KNApSAcK_search.py
@@ -22,19 +22,35 @@ print(url)
 urllib.request.urlretrieve(url, '%s.txt' % (x))
 
 
-# In[5]:
+# In[7]:
 
 ld = open("%s.txt" % (x))
 lines = ld.readlines()
 ld.close()
 
+C_number = []
+
 for line in lines:
     if line.find("word=C") >= 0:
         print(line[49:-34])
+        C_number.append(line[49:-34])
         urllib.request.urlretrieve("http://knapsack3d.sakura.ne.jp/mol3d/%s.3d.mol" % (line[49:-34]), '%s.mol' % (line[49:-34]))
+
+
+# In[8]:
+
+print(C_number)
+
+
+# In[12]:
+
+from rdkit import Chem
+mol_data = open('C00000730.mol','r').read()
+mol = Chem.MolFromMolBlock(mol_data)
+print(Chem.MolToSmiles(mol))
 
 
 # In[ ]:
 
-
+C=C(OC1C=C(C(=O)O)C=CC1N)C(=O)O
 
