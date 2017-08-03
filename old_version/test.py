949e49bc1898de488f98b3d2b312c4c93cf53a5e
sato-masayukidesu <maatanbeta@gmail.com>
2017-08-03 16:24:04 +0900

    oldversionの調整

--- old_version/test.py
+++ old_version/test.py
@@ -1,73 +1,78 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[2]:
 
 C_number = ["C00000730","C00000733","C00000734"]
 
 
-# In[ ]:
+# In[3]:
 
 from rdkit import Chem
 for i in C_number:
-    mol_data = open('%s.mol' % (i),'r').read()
+    mol_data = open('Klebsiella/%s.mol' % (i),'r').read()
     mol = Chem.MolFromMolBlock(mol_data)
     print(Chem.MolToSmiles(mol))
 
 
-# In[ ]:
+# In[4]:
 
 from rdkit.Chem import Recap
 from rdkit.Chem import Draw
-mol_data = open('%s.mol' % (C_number[0]),'r').read()
+mol_data = open('Klebsiella/%s.mol' % (C_number[0]),'r').read()
 mol = Chem.MolFromMolBlock(mol_data)
 node = Recap.RecapDecompose(mol)
 print(node)
 
 
-# In[ ]:
+# In[6]:
 
 all_nodes = [node.mol for node in node.GetAllChildren().values()]
 img = Draw.MolsToGridImage(all_nodes)
 img.save('all_nodes.png')
 
 
-# In[ ]:
+# フラグメント化して全部まとめて表示
+
+# In[7]:
 
 leaves = [leaf.mol for leaf in node.GetLeaves().values()]
 img = Draw.MolsToGridImage(leaves)
 img.save('leaves.png')
 
 
-# In[ ]:
-
-
-
+# 葉ノードだけ表示
 
 # In[ ]:
+
+
+
+
+# In[8]:
 
 from rdkit.Chem import AllChem
 from rdkit.Chem import BRICS
 from rdkit.Chem import rdMolDescriptors
 
 
-# In[ ]:
+# In[21]:
 
 smiles = Chem.MolToSmiles(mol)
 frag = BRICS.BRICSDecompose(mol)
+print(smiles)
 
 
-# In[ ]:
+# In[20]:
 
 print(len(frag))
 
 
-# In[ ]:
+# In[11]:
 
 print(frag)
 
 
-# In[ ]:
+# In[12]:
 
 from rdkit.Chem import Draw
 from rdkit.Chem import rdDepictor
@@ -82,21 +87,21 @@ for i in frag:
 
 
 
-# In[ ]:
+# In[16]:
 
 for i in C_number:
-    mol_data = open('%s.mol' % (i),'r').read()
+    mol_data = open('Klebsiella/%s.mol' % (i),'r').read()
     mol = Chem.MolFromMolBlock(mol_data)
     smiles = Chem.MolToSmiles(mol)
     frag = BRICS.BRICSDecompose(mol)
     print(frag)
 
 
-# In[ ]:
+# In[17]:
 
 mols = []
 for i in C_number:
-    mol_data = open('%s.mol' % (i),'r').read()
+    mol_data = open('Klebsiella/%s.mol' % (i),'r').read()
     mol = Chem.MolFromMolBlock(mol_data)
     mols.append(mol)
 outf = Chem.SDWriter("output.sdf")
@@ -105,7 +110,7 @@ for mol in mols:
     outf.write(mol)
 
 
-# In[ ]:
+# In[18]:
 
 from rdkit.Chem import rdFMCS
 ms = [m for m in Chem.SDMolSupplier('output.sdf') if m is not None]
@@ -116,6 +121,61 @@ for i,m in enumerate(ms):
     match_atoms = m.GetSubstructMatch(mcs_mol)
     print (match_atoms)
     Draw.MolToFile(m,'comp_%d.png' % i,highlightAtoms=match_atoms)
+
+
+# In[22]:
+
+print(mcs_smarts)
+
+
+# In[24]:
+
+mcsMol = Chem.MolFromSmarts(mcs_smarts,mergeHs=True)
+
+
+# In[25]:
+
+Draw.MolToFile(Chem.Mol(mcsMol.ToBinary()),"mcs.png",kekulize=False)
+
+
+# In[27]:
+
+match_list = []
+for i,m in enumerate(ms):
+    match_atoms = m.GetSubstructMatch(mcs_mol)
+    print (match_atoms)
+    match_list.append(match_atoms)
+img = Draw.MolsToGridImage(ms, highlightAtomLists=match_list)
+img.save('all_comp.png')
+
+
+# In[28]:
+
+with open("Streptomyces.kcfs")as f:
+    file = f.read()
+    molecule = file.split("///\n")
+    print(mocule[0])
+
+
+# In[7]:
+
+from rdkit.Chem import rdDepictor
+all_nodes = []
+for nod in node.GetAllChildren().values():
+    rdDepictor.Compute2DCoords(nod.mol)
+    all_nodes.append(nod.mol)
+img = Draw.MolsToGridImage(all_nodes)
+img.save('all_nodes.png')
+
+
+# In[9]:
+
+leavs = []
+for leaf in node.GetLeaves().values():
+    rdDepictor.Compute2DCoords(leaf.mol)
+    leavs.append(leaf.mol)
+img = Draw.MolsToGridImage(leavs)
+img.save('leaves.png')
 
 
 # In[ ]:
