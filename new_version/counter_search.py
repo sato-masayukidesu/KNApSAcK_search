2a4594c22c26130c419c140498abd0fa02791761
sato-masayukidesu <maatanbeta@gmail.com>
2017-08-08 17:22:06 +0900

    ラベルだけを作る関数を作成。

--- new_version/counter_search.py
+++ new_version/counter_search.py
@@ -19,7 +19,7 @@ def counter_search(gene, label):
     return re.findall(query, alltext)
 
 
-counter_search("Streptomyces", "C(C)-C(C)-C(C-C-C-C-C-C)-C-C-C-C-C-C-C-C-C")
+counter_search("Streptomyces", "C1-C1-C5-C1-C1-C5")
 
 
 def counter_search2(gene, label):
@@ -46,4 +46,22 @@ def counter_search2(gene, label):
         return Cnlist
 
 
-counter_search2("Streptomyces", "C-C-C-C-C-N-C-C-C-C-C-N")
+counter_search2("Streptomyces", "C1-C1-C5-C1-C1-C5")
+
+
+def make_label(label):
+    label_list = re.split("[-()]", label)
+    sep_list = re.split("[a-zA-Z][0-9]?[a-z]?", label)
+    query = ""
+    for i in range(label_list.count("")):
+        label_list.remove("")
+    for i in range(len(label_list)):
+        label_list[i] += "[0-9]?[a-z]?"
+    for l1, l2 in zip(sep_list, label_list):
+        query += l1 + l2
+    query = re.sub("\(", "[(]", query)
+    query = re.sub("\)", "[)]", query)
+    return query
+
+
+make_label("C1-C1-C5-C1-C1-C5")
