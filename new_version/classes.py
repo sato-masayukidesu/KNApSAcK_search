d258de9ae5cb6b9eb0a0ebed307ebc55a152b9d4
sato-masayukidesu <maatanbeta@gmail.com>
2017-09-28 16:42:45 +0900

    細かい修正とsimnetの追加

--- new_version/classes.py
+++ new_version/classes.py
@@ -11,6 +11,7 @@ class MCS_Finder(object):
         import requests
         import lxml.html
         import re
+        import networkx as nx
 
         self.genus = genus
         self.mol_list = None
@@ -306,6 +307,7 @@ class MCS_Finder(object):
         img.save(self.genus + "/" + filename)
         return True
 
+
     def make_image(self, label):
         self.mol_list = None
         Cnlist = self.get_Cnlist_from_label2(label)
@@ -330,3 +332,112 @@ class MCS_Finder(object):
                 if genus2 == self.genus:
                     genuses_list.append(element.text.replace("\xa0", " ")[:-1])
         return genuses_list
+
+    def make_graph(self, label, mode=0):
+        import networkx as nx
+        import matplotlib.pylab as plt
+        import pylab
+        import os
+
+        G = nx.Graph()
+        pylab.figure(figsize=(10, 10))
+        scoredic = dict()
+        Clist = self.get_Cnlist_from_label2(label)
+        for Cnumber in Clist:
+            filepath = "SIMCOMP/" + Cnumber + ".txt"
+            if not os.path.exists(filepath):
+                continue
+            with open(filepath, "r") as fi:
+                scorelist = []
+                score = fi.readline().split("\t")
+                score[1] = float(score[1][:-1])
+                if mode == 1:
+                    #特定のスコア値以上のエッジを作る
+                    while(score[1] > 0.9):
+                        scorelist.append(score)
+                        if score[0] in Clist and score[1] < 1:
+                            G = self.nxappend(G, Cnumber, score[0], score[1])
+                        score = fi.readline().split("\t")
+                        score[1] = float(score[1][:-1])
+                elif mode == 2:
+                    # 全部のエッジを作る。
+                    try:
+                        while(True):
+                            scorelist.append(score)
+                            if score[0] in Clist and score[1] < 1:
+                                G = self.nxappend(G, Cnumber, score[0], score[1])
+                            score = fi.readline().split("\t")
+                            score[1] = float(score[1][:-1])
+                    except IndexError:
+                        pass
+                elif mode == 3:
+                    # スコア値の高い順に二つ取ってくる 物によっては一つになっている。
+                    for i in range(3):
+                        scorelist.append(score)
+                        if score[0] in Clist and score[1] < 1:
+                            G = self.nxappend(G, Cnumber, score[0], score[1])
+                        score = fi.readline().split("\t")
+                        score[1] = float(score[1][:-1])
+                else:
+                    # エッジが二つになるように取ってくる
+                    i = 0
+                    try:
+                        while(True):
+                            scorelist.append(score)
+                            if score[0] in Clist and score[1] < 1:
+                                G = self.nxappend(G, Cnumber, score[0], score[1])
+                                i += 1
+                            score = fi.readline().split("\t")
+                            score[1] = float(score[1][:-1])
+                            if i >= 2:
+                                break
+                    except IndexError:
+                        pass
+                scoredic[Cnumber] = scorelist
+        print(str(len(G.nodes())) + "/" + str(len(Clist)))
+        print("edge:" + str(len(G.edges())))
+        # for g in nx.connected_component_subgraphs(G):
+        self.drawnx(G)
+        # nx.draw_networkx(G)
+        # plt.xticks([])
+        # plt.yticks([])
+        # plt.show()
+
+    def nxappend(self, G, start, end, weight):
+        if not start in G.nodes():
+            G.add_node(start)
+        if not end in G.nodes():
+            G.add_node(end)
+        if not (start, end) in G.edges() and not (end, start) in G.edges():
+            G.add_edge(start, end, weight=weight)
+        return G
+
+    def drawnx(self, G):
+        import networkx as nx
+        import matplotlib.pyplot as plt
+
+        pos = nx.spring_layout(G)
+        edge_labels = dict()
+        for param in G.edges(data=True):
+            edge_labels[(param[0], param[1])] = param[2]["weight"]
+        nx.draw_networkx_nodes(G, pos, node_size=200, node_color="w")
+        edge_width = [ d['weight'] for (u,v,d) in G.edges(data=True)]
+        nx.draw_networkx_edges(G, pos, width=edge_width)
+        # nx.draw_networkx_edges(G, pos)
+        # nx.draw_networkx_edge_labels(G, pos,edge_labels)
+        nx.draw_networkx_labels(G, pos ,font_size=10, font_color="r")
+        plt.xticks([])
+        plt.yticks([])
+
+    def get_simcomp(self, Clist, filename="SIMCOMP2/test.txt"):
+        import urllib
+        i = 0
+        url = "http://rest.genome.jp/simcomp2/"
+        urlC = ""
+        for Cnumber in Clist:
+            urlC += Cnumber + "+"
+        else:
+            urlC = urlC[:-1]
+        url += urlC + "/" + urlC + "/cutoff=0.1"
+        urllib.request.urlretrieve(url, filename)
+        return True
